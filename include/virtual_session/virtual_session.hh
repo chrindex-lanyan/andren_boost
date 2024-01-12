c5e74fb1501d26c30f5b22a0ecad35b2cebe875d
chrindex <chrindex.lanyan@yandex.com>
2024-01-13 00:29:35 +0800

    重新设计的简化的虚拟会话，带有状态

--- include/virtual_session/virtual_session.hh
+++ include/virtual_session/virtual_session.hh
@@ -1,90 +1,201 @@
 ﻿#pragma  once
 
+/**
+ * @file virtual_session.hh
+ * @author chrindex 
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ * 对于一个通信协议，需要考虑的事情很多。
+ *    1. 分包。通常可以选256，512，1024，2048，4096的大小.
+ *    2. 分流。
+ *          1. 一个连接可以维护多个虚拟连接。
+ *          2. 每个虚拟连接有着状态：{ 连接中，已连接，已断开 }。
+ *          3. 物理连接断开后，虚拟连接也要跟着断开。
+ *          4. 每个虚拟连接有着唯一标识，比如ID。
+ *          5. 每个虚拟连接不必分帧，只需要每次记录已发送/接收的数据大小即可。
+ *          6. 虚拟连接传送数据完成后直接断开（除非keep-alive），无需回应。
+ *          7. 虚拟连接发起数据传送需要被接收端确认才能开始发送。
+ *          8. 预留拓展字段。
+ */
+
+
 #include <cstdint>
+#include <functional>
 #include <string>
 #include <deque>
 #include <stdint.h>
 #include <memory>
 
-#include "virtual_session_manager_interface.hh"
-
 namespace chrindex::andren_boost
 {
-    class virtual_session 
+    #define MAGIC_NUM 0xffeeaadd11559900
+    #define DEFAULT_DATA_FRAME_SIZE 512
+
+    struct virtual_package_t
+    {
+        uint64_t magic_num = MAGIC_NUM;   // 魔术字
+        int16_t  package_size;            // 包大小
+        uint16_t type      ; // 包类型 { 建立会话, 请求, 响应, 传送, 拓展请求, 拓展响应  }
+        uint32_t vid : 24 ;  // 虚拟连接ID
+        uint32_t target_vid : 24; // 目标虚拟连接ID
+        union 
+        {
+            struct request_t
+            {
+                /*  type         content
+                 * ----------------------
+                 *  建立会话      数据传送大小  
+                 *  请求         {保持会话，断开连接}     
+                 *  拓展请求      用户自定义
+                 */
+                uint64_t content; 
+            } request;
+
+            struct response_t
+            {
+                /**
+                 * type         status
+                 * ---------------------
+                 * 响应      {已保持，不同意保持，已断开} 
+                 * 拓展响应   用户自定义
+                 */
+                uint64_t status;
+            } response;
+
+            struct transfer_data_request
+            {
+                /**
+                 * type     size    first
+                 * ---------------------------
+                 * 传送     数据大小  数据首字节
+                 */
+                uint16_t size;
+                char first;
+            } data;
+        } content;
+    };
+
+    enum class virtual_session_enum : int 
+    {
+        NONE = 0,               // (对接口)指示使用拓展字段{拓展请求、拓展响应}
+
+        SESSION_TYPE_SEND,      // 虚拟会话是数据接收模式
+        SESSION_TYPE_RECV,      // 虚拟会话是数据发送模式
+
+        PACKAGE_TYPE_CREATE,    // 包类型：创建虚拟会话
+        PACKAGE_TYPE_REQUEST ,  // 包类型：发送请求
+        PACKAGE_TYPE_RESPONSE , // 包类型：发送回应
+        PACKAGE_TYPE_TRANSFER,  // 包类型：数据传送
+        PACKAGE_TYPE_EXTREQ,    // 包类型：拓展请求
+        PACKAGE_TYPE_EXTRES,    // 包类型：拓展响应
+
+        REQUEST_CONTENT_KEEP_ALIVE, // 请求内容：保持连接不中断
+        REQUEST_CONTENT_DISCONNECT, // 请求内容：断开连接
+
+        RESPONSE_STATUS_KEEP_ALIVE, // 响应：同意保持连接
+        RESPONSE_STATUS_NOT_KEEP,   // 响应：不同意保持连接
+        RESPONSE_STATUS_DISCONNECT, // 响应：虚拟连接（对端）已经断开
+        RESPONSE_STATUS_ACCEPT,     // 响应：接收虚拟会话创建请求
+
+        STATUS_CLOSED,  // 会话状态：已关闭
+        STATUS_ONLINE,  // 会话状态：在线
+        STATUS_OPEN,    // 会话状态：已打开但不在线
+    };
+
+    class virtual_session : public std::enable_shared_from_this<virtual_session>
     {
     public :
 
-        using manager_type = 
-            std::weak_ptr<virtual_session_manager_interface>;
+        virtual_session();
+        virtual_session(virtual_session_enum type);
+        virtual_session(virtual_session && _ano) noexcept;
+        void operator=(virtual_session && _ano) noexcept;
+        virtual ~virtual_session();
 
-        virtual_session()
-        {
-            m_session_id = 0;
-            m_is_closed = true;
-        }
+        void operator=(virtual_session const & )= delete;
+        virtual_session(virtual_session const &) = delete;
 
-        virtual_session(uint64_t session_id , manager_type manager) noexcept 
-        { 
-            m_session_id = session_id;
-            m_is_closed = true;
-            m_manager_ptr = manager; 
-        }
+        uint32_t vid() const noexcept;
 
-        ~virtual_session()
-        {
-            if (m_is_closed == false)
-            {
-                
-            }
-        }
+        virtual_session_enum type() const noexcept;
 
-        /// 添加一个数据包到等待队列
-        bool append_write_request(std::string package) noexcept;
+        /////////// request /////////////
 
-        /// 接收一个数据包，如果有接收完成的数据包
-        std::string accept_package_if_no_empty() noexcept;
+        /// 建立虚拟会话
+        bool request_create(std::string data);
 
-        /// 数据包接收队列大小
-        size_t recv_queue_size() const noexcept;
+        /// 发送请求
+        bool send_request(virtual_session_enum type , uint64_t or_use_extention);
 
-        /// 数据包等待队列大小
-        size_t write_queue_size() const noexcept;
+        /// 发送响应
+        bool send_response(virtual_session_enum type , uint64_t or_use_extention);
 
-        /// 会话管理器的有效性
-        bool is_empty() const noexcept 
-        { 
-            auto manager = get_manager(); 
-            return !manager || manager->is_empty()
-                || manager->is_closed(); 
-        }
 
-        /// 会话的可用性
-        bool is_closed() const noexcept 
-        {
-            return is_empty() || m_is_closed == false;
-        }
+        using OnCreate = std::function<bool(virtual_session * session ,
+             uint64_t size)>;
 
-        uint64_t session_id() const noexcept;
+        using OnReqOrRes = std::function<void(virtual_session * session ,
+             virtual_session_enum type, uint64_t or_extention)>;
 
-        /// 提交到就绪队列以准备发送
-        bool try_commit() const noexcept;
+        using OnDisconnectd = std::function<void(virtual_session * session, 
+            bool forwardly)>;
 
-    private :
-
-        std::shared_ptr<virtual_session_manager_interface> 
-            get_manager() const noexcept 
-        {
-            return m_manager_ptr.lock();   
-        }
-
-    private :
-
-        uint64_t m_session_id;
-        bool m_is_closed;
-        manager_type m_manager_ptr;
+        /// 如果收到了会话建立
+        void setOnCreateReq(OnCreate callback);
         
-        std::deque<std::string> m_recv_package_list;
-        std::deque<std::string> m_write_package_list;
+        /// 如果收到了请求或者响应
+        void setOnReqOrRes(OnReqOrRes callback);
+
+        /// 如果连接断开(主动或被动)
+        void setOnDisconnected(OnDisconnectd callback);
+
+        /// 往里吐数据,解析,并尝试取出一个准备发送的数据
+        std::string working(std::string const & data , uint64_t & used_bytes);
+
+        /// 取出数据
+        /// 该操作不会删除缓冲区的数据
+        std::string fetch_data(size_t size);
+
+        /// 缓冲区内剩余的数据大小
+        size_t remanent_size();
+
+        /// 清空数据缓冲区,将已发送数据字段清零。
+        /// 该操作不会清空就绪队列。
+        /// 在你传送数据完成后仍然keep-alive且
+        /// 希望释放内存时使用。
+        void clear_buffer();
+
+        /// 设置数据帧最大大小
+        /// 函数只对数据发送类型的会话有效
+        void set_data_frame_max_size(uint16_t nbytes = DEFAULT_DATA_FRAME_SIZE);
+
+    private :
+
+        void send_next_data_frame();
+
+    private :
+
+        struct _context_t
+        {
+            uint32_t m_vid ;                     // 会话ID
+            uint32_t m_remote_vid;               // 对端会话ID
+            virtual_session_enum m_session_type; // 会话类型
+            virtual_session_enum m_status;       // 会话状态
+            size_t m_data_size;                  // 已传送的数据大小
+            uint16_t m_per_data_size;            // 每个数据帧的数据段最大大小 
+            bool m_keep_alive;                   // 是否保持连接(当数据传送完成后)
+            bool m_trans_start ;                 // 是否开始了数据传送
+            std::string m_buffer;                // 数据缓冲区
+            std::deque<std::string> m_next_send; // 下一个准备发送的数据
+            OnCreate m_callback_on_create; // 当收到建立连接的请求
+            OnReqOrRes m_callback_on_req_or_res; // 当收到其他请求或响应
+            OnDisconnectd m_callback_on_disconnect; // 当虚拟连接已断开
+        } ;
+        std::unique_ptr<_context_t> context;
     };
 
 }
---
+++ src/virtual_session/virtual_session.cpp
@@ -0,0 +1,505 @@
+﻿#include "virtual_session.hh"
+#include <cstdint>
+#include <memory>
+#include <atomic>
+#include <thread>
+
+thread_local std::atomic<uint64_t> virtual_session_id { 1 };
+
+static uint64_t create_virtual_session_id()
+{
+    uint64_t _tid = std::hash<std::thread::id>()(std::this_thread::get_id()) ;
+    _tid = (_tid << 32) + virtual_session_id++;
+    return _tid;
+}
+
+namespace chrindex::andren_boost
+{
+    virtual_session::virtual_session()
+    {
+        context = nullptr;
+    }
+
+    virtual_session::virtual_session(virtual_session_enum type)
+    {
+        context = std::make_unique<_context_t>();
+        context->m_session_type = type;
+        context->m_vid = create_virtual_session_id() & 0x00ffffff;
+        context->m_remote_vid = 0;
+        context->m_data_size = 0;
+        context->m_keep_alive = false;
+        context->m_trans_start = false;
+        context->m_per_data_size = DEFAULT_DATA_FRAME_SIZE;
+        context->m_status = virtual_session_enum::STATUS_OPEN;
+    }
+
+    virtual_session::virtual_session(virtual_session && _ano) noexcept
+    {
+        context = std::move(_ano.context);
+    }
+
+    void virtual_session::operator=(virtual_session && _ano) noexcept
+    {
+        this->~virtual_session();
+        context = std::move(_ano.context);
+    }
+
+    virtual_session::~virtual_session()
+    {
+        if (context && context->m_status 
+                == virtual_session_enum::STATUS_ONLINE)
+        {
+            send_response(virtual_session_enum::
+                RESPONSE_STATUS_DISCONNECT,0);
+            if (context->m_callback_on_disconnect)
+            {
+                context->m_callback_on_disconnect(this,true);
+            }
+        }
+    }
+
+
+    uint32_t virtual_session::vid() const noexcept
+    {
+        return context == nullptr ? 0 : context->m_vid;
+    }
+
+    virtual_session_enum virtual_session::type() const noexcept
+    {
+        return context == nullptr ? 
+            virtual_session_enum::NONE : context->m_session_type;
+    }
+
+    /////////// request /////////////
+
+    /// 建立虚拟会话
+    bool virtual_session::request_create(std::string data)
+    {
+        if (!context || context->m_status 
+            != virtual_session_enum::STATUS_OPEN)
+        {
+            return false;
+        }
+
+        std::string _package(sizeof(virtual_package_t),0x00);
+        virtual_package_t & package = 
+            *reinterpret_cast<virtual_package_t*>(&_package[0]);
+
+        package.magic_num = MAGIC_NUM;
+        package.package_size = sizeof(package);
+        package.type = (uint16_t)virtual_session_enum::PACKAGE_TYPE_CREATE;
+        package.vid = vid();
+        package.target_vid = 0;
+        package.content.request.content = data.size();
+
+        context->m_buffer = std::move(data);
+        context->m_data_size = 0;
+        context->m_next_send.clear();
+
+        context->m_next_send.push_back(_package);
+        return true;
+    }
+
+    /// 发送请求
+    bool virtual_session::send_request(virtual_session_enum type ,
+        uint64_t or_use_extention)
+    {
+        if (!context || context->m_status 
+            != virtual_session_enum::STATUS_ONLINE)
+        {
+            return false;
+        }
+        std::string _package(sizeof(virtual_package_t),0x00);
+        virtual_package_t & package = 
+            *reinterpret_cast<virtual_package_t*>(&_package[0]);
+
+        package.magic_num = MAGIC_NUM;
+        package.package_size = sizeof(package);
+        package.type = (
+            type == virtual_session_enum::NONE ?
+                (uint16_t)virtual_session_enum::PACKAGE_TYPE_EXTREQ
+            :   (uint16_t)virtual_session_enum::PACKAGE_TYPE_REQUEST);
+        package.vid = vid();
+        package.target_vid = context->m_remote_vid;
+        package.content.request.content = (
+            type == virtual_session_enum::NONE ?
+                (uint64_t)or_use_extention
+            :   (uint64_t)type );
+        
+        context->m_next_send.push_back(_package);
+        return true;
+    }
+
+    /// 发送响应
+    bool virtual_session::send_response(virtual_session_enum type , 
+        uint64_t or_use_extention)
+    {
+        if (!context || context->m_status 
+            != virtual_session_enum::STATUS_ONLINE)
+        {
+            return false;
+        }
+        std::string _package(sizeof(virtual_package_t),0x00);
+        virtual_package_t & package = 
+            *reinterpret_cast<virtual_package_t*>(&_package[0]);
+
+        package.magic_num = MAGIC_NUM;
+        package.package_size = sizeof(package);
+        package.type = ( type == virtual_session_enum::NONE ?
+            (uint16_t)virtual_session_enum::PACKAGE_TYPE_EXTRES
+            :(uint16_t)virtual_session_enum::PACKAGE_TYPE_RESPONSE);
+        package.vid = vid();
+        package.target_vid = context->m_remote_vid;
+        package.content.response.status = (type == virtual_session_enum::NONE ?
+            or_use_extention
+            : (uint64_t) type
+            );
+        
+        context->m_next_send.push_back(_package);
+        return true;
+    }
+
+    /// 如果收到了会话建立
+    void virtual_session::setOnCreateReq(OnCreate callback)
+    {
+        if (context)
+        {
+            context->m_callback_on_create = std::move(callback);
+        }   
+    }
+    
+    /// 如果收到了请求或者响应
+    void virtual_session::setOnReqOrRes(OnReqOrRes callback)
+    {
+        if (context)
+        {
+            context->m_callback_on_req_or_res 
+                = std::move(callback);
+        }    
+    }
+
+    /// 如果连接断开(主动或被动)
+    void virtual_session::setOnDisconnected(OnDisconnectd callback)
+    {
+        if (context)
+        {
+            context->m_callback_on_disconnect = std::move(callback);
+        }
+    }
+
+    /// 往里吐数据,解析,并尝试取出一个准备发送的数据
+    std::string virtual_session::working(
+        std::string const & data,uint64_t & used_bytes)
+    {
+        used_bytes = 0;
+
+        if (context==nullptr)
+        {
+            return {};
+        }
+
+        // 处理数据帧
+        send_next_data_frame();
+        
+        auto _func = [this]()->std::string
+        {
+            if (context->m_next_send.empty() == false)
+            {
+                std::string _next_send = 
+                    std::move(context->m_next_send.front());
+                context->m_next_send.pop_front();
+                return _next_send;
+            }
+            return {};
+        };
+
+        if(data.empty())
+        {
+            return _func();
+        }
+
+        if (data.size() != sizeof(virtual_package_t))
+        {
+            return _func();   
+        }
+
+        virtual_package_t const & package =
+            *reinterpret_cast<virtual_package_t const*>(data.c_str());
+        
+        if (package.magic_num != MAGIC_NUM 
+            || package.package_size > data.size())
+        {
+            return _func(); 
+        }
+
+        virtual_session_enum package_type =
+            (virtual_session_enum)package.type;
+
+        switch (package_type) 
+        {
+            case virtual_session_enum::PACKAGE_TYPE_CREATE: 
+            {
+                if(context->m_callback_on_create)
+                {
+                    bool bret = context->m_callback_on_create(this,
+                        package.content.request.content);
+                    if (bret) // 是否同意
+                    {
+                        context->m_remote_vid = package.vid;
+                        context->m_buffer.resize(
+                            package.content.request.content);
+                        context->m_status = 
+                            virtual_session_enum::STATUS_ONLINE;
+                        
+                        context->m_trans_start = true;
+                        // 发送response
+                        send_response(virtual_session_enum::RESPONSE_STATUS_ACCEPT,
+                        0);
+                    }
+                }
+                break;
+            };
+            case virtual_session_enum::PACKAGE_TYPE_EXTREQ: 
+            {
+                if(context->m_callback_on_create)
+                {
+                    context->m_callback_on_req_or_res(
+                        this, package_type, 
+                        package.content.request.content);
+                }
+                break;
+            };
+            case virtual_session_enum::PACKAGE_TYPE_EXTRES: 
+            {
+                if(context->m_callback_on_create)
+                {
+                    context->m_callback_on_req_or_res(
+                        this, package_type, 
+                        package.content.response.status);
+                }
+                break;
+            };
+            case virtual_session_enum::PACKAGE_TYPE_REQUEST: 
+            {
+                if (package.content.request.content 
+                    == (uint64_t) 
+                    virtual_session_enum::REQUEST_CONTENT_DISCONNECT)
+                {
+                    if(context->m_callback_on_disconnect)
+                    {
+                        context->m_callback_on_disconnect(
+                            this, false);
+                    } 
+                }
+                else if (package.content.request.content 
+                    == (uint64_t) 
+                        virtual_session_enum::REQUEST_CONTENT_KEEP_ALIVE)
+                {
+                    if (context->m_callback_on_req_or_res)
+                    {
+                        context->m_callback_on_req_or_res(
+                            this,
+                            (virtual_session_enum)
+                                package.content.request.content ,
+                            0
+                        );
+                    }
+                }
+                
+                break;
+            };
+            case virtual_session_enum::PACKAGE_TYPE_RESPONSE: 
+            {
+                /**
+                 *  RESPONSE_STATUS_KEEP_ALIVE, // 响应：同意保持连接
+                    RESPONSE_STATUS_NOT_KEEP,   // 响应：不同意保持连接
+                    RESPONSE_STATUS_DISCONNECT, // 响应：虚拟连接（对端）已经断开
+                 */
+                if (package.content.response.status 
+                    == (uint64_t) virtual_session_enum::RESPONSE_STATUS_DISCONNECT)
+                {
+                    if(context->m_callback_on_disconnect)
+                    {
+                        context->m_callback_on_disconnect(
+                            this, false);
+                    }
+                    break;
+                }
+                else if (package.content.response.status 
+                    == (uint64_t) virtual_session_enum::RESPONSE_STATUS_NOT_KEEP)
+                {   
+                    context->m_keep_alive = false;
+                }
+                else if (package.content.response.status 
+                    == (uint64_t) virtual_session_enum::RESPONSE_STATUS_KEEP_ALIVE)
+                {
+                    context->m_keep_alive = true;
+                }
+                else if(package.content.response.status 
+                    == (uint64_t)virtual_session_enum::RESPONSE_STATUS_ACCEPT)
+                {
+                    context->m_remote_vid = package.vid;
+                    context->m_trans_start = true;
+                }
+                if(context->m_callback_on_req_or_res)
+                {
+                    context->m_callback_on_req_or_res(
+                        this, package_type, 
+                        0);
+                }
+                break;
+            };
+            case virtual_session_enum::PACKAGE_TYPE_TRANSFER: 
+            {
+                char const * _pdata = &package.content.data.first;
+
+                if (context->m_buffer.size() < context->m_data_size)
+                {
+                    context->m_buffer.resize(context->m_data_size);
+                }
+
+                for (uint32_t i = 0 ; i < package.content.data.size 
+                    && context->m_data_size > package.content.data.size ;
+                    i++)
+                {
+                    context->m_buffer[context->m_data_size + i]
+                        = _pdata[i];
+                }
+                context->m_data_size += package.content.data.size;
+                
+                used_bytes = package.package_size;
+                break;
+            };
+            default:
+            {
+                used_bytes = 0;
+                return _func(); 
+            }
+        }
+
+        used_bytes = 0;
+        return {};
+    }
+
+    void virtual_session::send_next_data_frame()
+    {
+        if ( !context->m_trans_start ||
+            context->m_session_type 
+                != virtual_session_enum::SESSION_TYPE_SEND)
+        {
+            return ;
+        }
+        std::string data(sizeof(virtual_package_t),0x00) ;
+
+        virtual_package_t & package =
+            *reinterpret_cast<virtual_package_t*>(&data[0]);
+
+        package.magic_num = MAGIC_NUM;
+        package.target_vid = context->m_remote_vid;
+        package.vid = context->m_vid;
+        package.type = (uint16_t)virtual_session_enum::PACKAGE_TYPE_TRANSFER;
+
+        // 没发完
+        if (context->m_data_size + 
+            context->m_per_data_size < context->m_buffer.size())
+        {
+            data.insert(data.begin() + sizeof(virtual_package_t) , 
+                context->m_buffer.begin() + context->m_data_size,
+                context->m_buffer.begin() + context->m_data_size 
+                    + context->m_per_data_size);
+            context->m_data_size += context->m_per_data_size;
+            package.content.data.size = context->m_per_data_size;
+            package.package_size = data.size();
+            context->m_next_send.push_back(std::move(data));
+        }
+        else  // 能够发完
+        {
+            size_t size = std::min(
+                context->m_buffer.size() - context->m_data_size,
+                (uint64_t)context->m_per_data_size);
+            data.insert(data.begin() + sizeof(virtual_package_t) ,
+                context->m_buffer.begin() + context->m_data_size,
+                context->m_buffer.begin() + context->m_data_size + size);
+            context->m_data_size += size;
+            package.content.data.size = size;
+            package.package_size = data.size();
+            context->m_next_send.push_back(std::move(data));
+            if (context->m_keep_alive == false)
+            {
+                send_response(virtual_session_enum::RESPONSE_STATUS_DISCONNECT, 
+                    0);
+                if (context->m_callback_on_disconnect)
+                {
+                    context->m_callback_on_disconnect(this,true);
+                }
+            }
+        }
+    }
+
+    /// 取出数据
+    std::string virtual_session::fetch_data(size_t size)
+    {
+        size_t _size = 0;
+        std::string _data;
+
+        if (context->m_session_type == 
+            virtual_session_enum::SESSION_TYPE_SEND)
+        {
+            _size = context->m_buffer.size() - context->m_data_size;
+            _size = std::min(size,_size);
+            size_t need = context->m_buffer.size() - _size;
+            _data.insert(_data.begin(),
+                context->m_buffer.begin() + need, 
+                context->m_buffer.begin() + _size);
+        }
+        else if (context->m_session_type == 
+            virtual_session_enum::SESSION_TYPE_RECV)
+        {
+            _size = context->m_data_size;
+            _data.insert(_data.begin(),
+                context->m_buffer.begin(),
+                context->m_buffer.begin() + _size);
+        }
+        return _data;
+    }
+
+    /// 缓冲区内剩余的数据大小
+    size_t virtual_session::remanent_size()
+    {
+        if (context==nullptr)
+        {
+            return 0;
+        }
+        if (context->m_session_type == 
+            virtual_session_enum::SESSION_TYPE_SEND)
+        {
+            return context->m_buffer.size() - context->m_data_size;
+        }
+        else if (context->m_session_type == 
+            virtual_session_enum::SESSION_TYPE_RECV)
+        {
+            return context->m_data_size;
+        }
+        return 0;
+    }
+
+    void virtual_session::clear_buffer()
+    {
+        if (context)
+        {
+            context->m_buffer.clear();
+            context->m_data_size = 0;
+        }
+    }
+
+    void virtual_session::set_data_frame_max_size(uint16_t nbytes )
+    {
+        if(context)
+        {
+            context->m_per_data_size = 
+                std::min((uint16_t)8192 , 
+                    std::max( (uint16_t)128, nbytes));
+        }
+    }
+
+}
